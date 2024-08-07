dc203f2e95774e4c1e23b20fa7155b1c110cf378
pheda <pheda@sscinc.com>
2024-08-06 23:19:05 -0700

    All updated to latest

--- commands.sh
+++ commands.sh
@@ -4,14 +4,21 @@ docker images --format "{{.Repository}}"
 # Remove dangling images
 docker rmi $(docker images -q -f "dangling=true")
 
-docker images  | grep -v -e redis -e postgres:13
-docker images  | grep -v redis
+docker images  | grep -v -e redis -e postgres -e python
+docker images  | grep redis
 
-docker rmi $(docker images -q | grep -v -e redis -e postgres:13)
+docker rmi $(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v -e redis -e postgres -e python)
+
+images_to_remove=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v -e redis -e postgres -e python); [ -n "$images_to_remove" ] && docker rmi $images_to_remove || echo "No images to remove"
+# delete all images other than these specific tags
+images_to_remove=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -v -E '^(redis:latest|postgres:15|python:3.10-slim-buster|python:3.9-slim-buster)$'); [ -n "$images_to_remove" ] && docker rmi $images_to_remove || echo "No images to remove"
+
+
+
 docker ps
 docker images
 
-docker pull postgres:13
+docker pull postgres:15
 docker pull redis
 docker pull python:3.10-slim-buster
 docker pull python:3.9-slim-buster
@@ -20,6 +27,10 @@ docker pull python:3.9-slim-buster
 docker compose build postgres
 docker compose build service2
 docker compose build service3
+docker compose build service3
 docker compose up
 
-docker-compose build service3
+docker compose build service3
+
+
+docker logs 
